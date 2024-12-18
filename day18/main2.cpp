b88d6e262e4192ddfcfc363d1f3111157321a048
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2024-12-19 00:15:10 +0530

    donee even tho slightly late

--- day18/main2.cpp
+++ day18/main2.cpp
@@ -9,13 +9,15 @@
 std::vector<std::vector<bool>> ogfallen(SIZE,std::vector<bool>(SIZE,0));
 std::vector<std::vector<bool>> fallen(SIZE,std::vector<bool>(SIZE,0));
 int val(int i,int j) {
-    if (i<0 || j>=SIZE) return 0;
-    if (j<0 || i>=SIZE) return 1;
+    if (i<0 || j>=SIZE) return 1;
+    if (j<0 || i>=SIZE) return 0;
     if (!fallen[i][j]) return 0;
     fallen[i][j]=0;
     for (int x=-1;x<2;x++) for (int y=-1;y<2;y++) {
         if (x!=0 || y!=0) {
-            if (val(x+i,y+j)) return 1;
+            if (val(x+i,y+j)) {
+                return 1;
+            }
         }
     }
     return 0;
@@ -23,7 +25,8 @@ int val(int i,int j) {
 int main(int argc,char**argv) {
     std::ifstream file(argv[1]);
     int ans;
-    for (ans=1;file.good();ans++) {
+    int run=1;
+    for (ans=0;file.good()&&run;ans++) {
         char c;
         int x=0,y=0;
         file>>x;
@@ -32,11 +35,17 @@ int main(int argc,char**argv) {
         ogfallen[x][y]=1;
         fallen=ogfallen;
         for (int i=0;i<SIZE-1;i++) {
-            if (fallen[i][0] && val(i,0)) break;
-            if (fallen[SIZE-1][i] && val(SIZE-1,i)) break;
+            if (val(i,0)) {
+                std::cout << x << "," << y << std::endl;
+                run=0;
+                break;
+            }
+            if (val(SIZE-1,i)) {
+                std::cout << x << "," << y << std::endl;
+                run=0;
+                break;
+            }
         }
     }
-    if (file.good()) std::cout << ans;
-    else std::cout << "ohno" << std::endl;
     file.close();
 }
